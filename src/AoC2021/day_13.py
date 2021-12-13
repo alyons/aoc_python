93e49d73b457b28299f0898fc49a78ac94b3e73d
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-12-13 08:33:56 -0500

    chore: remove unused function

--- src/AoC2021/day_13.py
+++ src/AoC2021/day_13.py
@@ -1,25 +1,6 @@
 from typing import List, Tuple
 
 
-def perform_fold(points: List[List[int]], fold: Tuple[str, int]):
-    axis, line = fold
-
-    # Find points to fold
-    to_move = [p for p in points if (axis == 'x' and p[0] > line) or (axis == 'y' and p[1] > line)]
-
-    # Remove points which will be moved and move the points
-    for m in to_move:
-        points.remove(m)
-    
-        # could be m[axis == 'y'] -= 2 *abs(m[axis == 'y'] - line) but that's not legible
-        if axis == 'x':
-            m[0] -= 2 * abs(m[0] - line)
-        elif axis == 'y':
-            m[1] -= 2 * abs(m[1] - line)
-        
-        if not m in points: points.append(m)
-
-
 def perform_folds(points: List[List[int]], folds: List[Tuple[str, int]]):
     for point in points:
         for fold in folds:
--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -1,13 +1,7 @@
 from AoC2021.util import parse_manual
-from AoC2021.day_13 import perform_fold, perform_folds
+from AoC2021.day_13 import perform_folds
 
 points, folds = parse_manual('./tests/data/day_13.txt')
-
-
-def test_perform_fold():
-    _points = points.copy()
-    perform_fold(_points, folds[0])
-    assert len(_points) == 17
 
 
 def test_perform_folds():
